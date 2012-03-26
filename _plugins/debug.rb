9a1652e20b8cd1cae0315d02d16e4c59b9539ae5
equation85 <frank85chen@gmail.com>
2012-03-26 22:08:24 +0800

    little changes

--- _plugins/debug.rb
+++ _plugins/debug.rb
@@ -3,13 +3,13 @@
 #  Can be used anywhere liquid syntax is parsed (templates, includes, posts/pages)
 #  {{ site | debug }}
 #  {{ site.posts | debug }}
-#
+
 require 'pp'
 module Jekyll
   # Need to overwrite the inspect method here because the original
   # uses < > to encapsulate the psuedo post/page objects in which case
   # the output is taken for HTML tags and hidden from view.
-  #
+  
   class Post
     def inspect
       "#Jekyll:Post @id=#{self.id.inspect}"
